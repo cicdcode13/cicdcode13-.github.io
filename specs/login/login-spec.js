76d694ea3018ede0a699320f4b57ec49a087b51f
Sergii Khromchenko <khromchenko13@gmail.com>
2021-12-19 18:48:40 -0500

    new test case

--- specs/login/login-spec.js
+++ specs/login/login-spec.js
@@ -9,4 +9,8 @@ describe('Login', () => {
     it('should be able to login with matching credentials', () => {
         // This test will always pass as there is no code to fail
     })
+
+    it('should not be able to login with no password', () => {
+        // This test will always pass as there is no code to fail
+    })
 })
