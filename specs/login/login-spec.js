171e2210def343cdce81b98dae52953fced6ee02
Sergii Khromchenko <khromchenko13@gmail.com>
2021-12-19 19:19:13 -0500

    add no username and password attempt test coverage

--- specs/login/login-spec.js
+++ specs/login/login-spec.js
@@ -1,6 +1,3 @@
-const loginUrl = 'https://www.instagram.com/accounts/login/?source=auth_switcher';
-const timeout = 20000;
-
 describe('Login', () => {
     it('should not be able to login with none matching credentials', () => {
         // This test will always pass as there is no code to fail
@@ -15,6 +12,10 @@ describe('Login', () => {
     })
 
     it('should not be able to login with no username', () => {
+        // This test will always pass as there is no code to fail
+    })
+
+    it('should not be able to login with no username and password', () => {
         // This test will always pass as there is no code to fail
     })
 })
