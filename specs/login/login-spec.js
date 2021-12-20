300b057f0fcc34266a51aedc826b2cd8fe90b211
Sergii Khromchenko <khromchenko13@gmail.com>
2021-12-19 19:29:32 -0500

    add logout tst

--- specs/login/login-spec.js
+++ specs/login/login-spec.js
@@ -18,4 +18,8 @@ describe('Login', () => {
     it('should not be able to login with no username and password', () => {
         // This test will always pass as there is no code to fail
     })
+
+    it('should be able to logout', () => {
+        // This test will always pass as there is no code to fail
+    })
 })
